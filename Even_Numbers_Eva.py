c2cd812c63a619322066764191b1da651898cda3
EvaAmmann <eva.m.ammann@gmail.com>
2020-03-06 15:05:32 +0100

    PyCharm happy with layout of the code

--- Even_Numbers_Eva.py
+++ Even_Numbers_Eva.py
@@ -1,18 +1,20 @@
-#Function has to be defined, vals is a freely chosen word
+# Function has to be defined, vals is a freely chosen word
 def count_even(vals):
     """Returns the amount of even numbers."""
-    #counting variable defined
-    c=0
-    #loop, v is a freely chosen variable
+    # counting variable defined
+    c = 0
+    # loop, v is a freely chosen variable
     for v in vals:
-        #% is modulus
+        # % is modulus
         if (v % 2) == 0:
             # counting variable
-            c=c+1
+            c = c + 1
     return c
-#list
-x = [-2, -4, 3, 6, 88, 76, -7]
-#only call the function now! It was defined before
-y=count_even(x)
 
-print("The list {} contains {} even numbers.".format(x,y))
+
+# list
+x = [-2, -4, 3, 6, 88, 76, -7]
+# only call the function now! It was defined before
+y = count_even(x)
+
+print("The list {} contains {} even numbers.".format(x, y))
