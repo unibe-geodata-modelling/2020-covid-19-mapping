b3bd6145aaf22905eb3f0efc394daf98b2fa4133
EvaAmmann <eva.m.ammann@gmail.com>
2020-04-17 14:45:37 +0200

    Make dates out of column headers and create three days in the loop

--- Covid_WorldMap.py
+++ Covid_WorldMap.py
@@ -4,17 +4,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import datetime
 from datetime import date
 import urllib.request
 # Ask how to import GDAL packages etc. cartopy
 #import cartopy.crs as ccrs
 
 
-
 # Get Data locally (2nd of April 2020)
 #inputdata = "/Users/evaammann/Dropbox/Eva Ammann - Universität/universität bern/Master/Geographie/FS 2020/Seminar Geodatenanalyse/PyCharmProjects/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
-# Get Data from John Hopkins GitHub Repository
+# Get Data from John Hopkins GitHub Repository --> RAW very important
 inputdata = urllib.request.urlopen("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 
 #df_world = dataframe worldwide
@@ -56,16 +56,16 @@ print(country_and_province)
 
 
 # Dataframe for Switzerland
-df_Switzerland = df_world[df_world.Country == "Switzerland"]
-print(df_Switzerland)
+#df_Switzerland = df_world[df_world.Country == "Switzerland"]
+#print(df_Switzerland)
 
 # Dataframes Europe
-df_France = df_world[df_world.Country == "France"]
-df_Germany = df_world[df_world.Country == "Germany"]
-df_Italy = df_world[df_world.Country == "Italy"]
-print (df_France)
-print(df_Germany)
-print(df_Italy)
+#df_France = df_world[df_world.Country == "France"]
+#df_Germany = df_world[df_world.Country == "Germany"]
+#df_Italy = df_world[df_world.Country == "Italy"]
+#print (df_France)
+#print(df_Germany)
+#print(df_Italy)
 
 
 #Count the number of rows
@@ -87,17 +87,39 @@ for day in date_list:
         else:
             province = df_world.at[row, 'Province']
             print(province)
-        #naming = 'Switzerland' + date
-        Switzerland_March = df_world.at[row,day]
-        print (Switzerland_March)
-#Maybe create loops for days and location?
-#Data for 1/22/20
-#for location in country_and_province:
+        confirmed_cases = df_world.at[row,day]
+        print (confirmed_cases)
 
-#Create list with all dates
+for day in date_list:
+    date_fractions = day.split('/')
+    month = date_fractions[0]
+    day_of_month = date_fractions[1]
+    year = "20" + date_fractions[2]
+    print ("day = " + day,"year = " + year, "month = " + month, "day_of_the_month = " + day_of_month)
+    day1 = datetime.date(int(year),int(month),int(day_of_month))
+    day0 = day1 - datetime.timedelta(days=1)
+    day2 = day1 + datetime.timedelta(days=1)
+    print(day0)
+    print(day1)
+    print(day2)
+    #print ("This is the day " + day)
+    #Loop over all rows (Countries and Provinces)
+    for row in total_rows_list:
+        province = df_world.at[row, 'Province']
+        if pd.isna(province):
+            country = df_world.at[row, 'Country']
+            #print(country)
+        else:
+            province = df_world.at[row, 'Province']
+            #print(province)
+        confirmed_cases = df_world.at[row,day]
+        #print (confirmed_cases)
+
+
+
+#for location in country_and_province:
 
 
 #ax = plt.axes(projection =ccrs.PlateCarree())
 #ax.coastlines()
 #plt.show()
-# Still to do: Nicer showing of the graph, I just want enitre number ticks, I do not want to define the maximum and automated date creation! Otherwise very nice :D
