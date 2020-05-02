b13f00d6e09a6a55b843da777bb7972098a7f141
EvaAmmann <eva.m.ammann@gmail.com>
2020-05-02 10:11:41 +0200

    Commit 2.05.2020_v2

--- Covid_WorldMap.py
+++ Covid_WorldMap.py
@@ -197,3 +197,4 @@ for date_now in date_list_adjusted:
     plt.show()
     # plt.text(df_world_threedayaverage.at[location, 'Lon'], df_world_threedayaverage.at[location, 'Lat'],
     # 'yay' , horizontalalignment='right', transform=ccrs.PlateCarree())
+#wheredoIcommit?
